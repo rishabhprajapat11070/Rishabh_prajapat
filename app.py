36f41234203c999a485353f8bec3045cd0584bff
rishabhprajapat11070 <rp892422@gmail.com>
2026-04-12 19:22:50 +0530

    app and repquerment is updated

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template,request,Response,redirect,url_for
+import os
 import time
 
 app = Flask(__name__)
@@ -20,5 +21,7 @@ def Submit():
 
 
 
-if __name__=="__main__":
-    app.run(debug=True)
+port = int(os.environ.get("PORT", 5000))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=port)
